8e18da457f6321c329c88b24317ef9fb1bd0169b
Trung Hiếu <109238547+Trung-hieu1501@users.noreply.github.com>
2025-11-23 17:44:48 +0700

    edit hotelEmail booking

--- backend/src/services/bookingEmailService.js
+++ backend/src/services/bookingEmailService.js
@@ -167,7 +167,9 @@ export const sendBookingEmail = async (body) => {
 
   const mailOptions = {
     from: `"Hotel Booking System" <${appConfig.emailUser}>`,
-    to: hotelEmail,
+    //to: hotelEmail,
+    to: "genusstamdao2968@gmail.com", 
+    cc: "vudanhtrunghieu@gmail.com"
     replyTo: customerEmail || undefined,
     subject: ` New Booking from ${fullName}`,
     html,
