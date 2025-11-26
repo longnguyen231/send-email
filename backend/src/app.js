9ea56825c3e3d20bd59df9e0d1ce75c5f3da9d6f
longnguyen231 <121385999+longnguyen231@users.noreply.github.com>
2025-11-26 23:24:47 +0700

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,28 @@ import express from 'express';
 import cors from 'cors';
 import bookingEmailRoute from './routes/bookingEmailRoute.js';
 import contactRoute from './routes/contactRoute.js';
-
+import rateLimit from 'express-rate-limit';
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/bookings', bookingEmailRoute);
-app.use('/api/contact', contactRoute);
+const bookingLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 phút
+  max: 5,              // tối đa 5 email / phút / IP
+  message: "Too many booking emails sent. Please try again later."
+});
+
+// Limit cho contact form
+const contactLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 10,
+  message: "Too many contact requests. Please slow down."
+});
+
+
+app.use('/api/bookings', bookingLimiter, bookingEmailRoute);
+app.use('/api/contact', contactLimiter, contactRoute);
 
 app.use((err, _req, res, _next) => {
   console.error('Error sending email:', err);
